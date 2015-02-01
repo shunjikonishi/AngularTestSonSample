0f89eacc5562b896e1b89ea105800cef59b80cd4
Shunji Konishi <shun.konishi@nifty.com>
2015-02-01 15:30:41 +0900

    Add test for cartItem

--- client/app/cartItem/cartItem.service.spec.js
+++ client/app/cartItem/cartItem.service.spec.js
@@ -1,6 +1,28 @@
 'use strict';
 
 describe('Service: cartItem', function () {
+  var products = [{
+    "productId": 10,
+    "productName": "はじめてのAngularJS(和書)",
+    "price": 2000
+  },{
+    "productId": 11,
+    "productName": "AngularJSリファレンス 単行本（ソフトカバー）",
+    "price": 3500
+  },{
+    "productId": 12,
+    "productName": "スコンブ(業務用)",
+    "price": 800
+  },{
+    "productId": 13,
+    "productName": "味わいカルピス",
+    "price": 100
+  }];
+  function addAllProducts() {
+    for (var item in products) {
+      cartItem.add(item);
+    }
+  }
 
   // load the service's module
   beforeEach(module('AngularJsTestson'));
@@ -11,8 +33,37 @@ describe('Service: cartItem', function () {
     cartItem = _cartItem_;
   }));
 
-  it('should do something', function () {
-    expect(!!cartItem).to.be.true;
+  it('Initial length should be 0', function () {
+    expect(cartItem.items.length).to.be.eql(0);
   });
 
+  it('Test for add', function () {
+    addAllProducts();
+    expect(cartItem.items.length).to.be.eql(products.length);
+  });
+
+  /*
+  // this method doesn't work
+  it('Test for remove', function () {
+    addAllProducts();
+    cartItem.remove(products[1]);
+    expect(cartItem.items.length).to.be.eql(products.length - 1);
+  });
+  */
+
+  it('Test for clear', function () {
+    addAllProducts();
+    cartItem.clear();
+    expect(cartItem.items.length).to.be.eql(0);
+  });
+
+  it('Test for removeProductId', function () {
+    for (var i=0; i<3; i++) {
+      cartItem.add(products[0]);
+    }
+    expect(cartItem.items.length).to.be.eql(3);
+
+    cartItem.removeProductId(products[0].productId);
+    expect(cartItem.items.length).to.be.eql(2);
+  });
 });
